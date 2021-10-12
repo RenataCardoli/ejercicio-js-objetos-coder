a232d752eefc9814902a8ee1dfcd66fc9fa42a28
Renata Cardoso <renata.cardoli@gmail.com>
2021-10-12 16:47:10 -0300

    primero commit

---
+++ main.js
@@ -0,0 +1,50 @@
+
+/* Esta es una funcion de primera clase, miintencion es que trabaje como una class de tipo constructor, 
+lo que hace es setear al nombre dos propriedades adicionales done estan puntos de vida e punto extra.*/
+
+function Jugador(nombre){
+    this.nombre = nombre;
+    this.puntosVida = 100;
+    this.puntoExtra = 100;
+
+/*Este metodo llamado curar que recibe un jugadorAccion por parametro suma puntos de vida.
+Este metodo llamado atacar que recibe un jugadorAccion por parametro retira puntos de vida.
+
+Esa misma funcion de primera clase quien se encarga de imprimir este jugador por referencia a que hago decisiones sensillas o acciones simples*/
+
+
+    this.curar = function(jugadorAccion){
+        if(this.puntoExtra >= 40 ){
+            this.puntoExtra -=40;
+            jugadorAccion.puntosVida = jugadorAccion.puntosVida + 20;
+        }else{
+            console.info( this.nombre + " No tiene puntos extras")
+        }
+        this.estado(jugadorAccion);
+    }
+
+    this.atacar = function(jugadorAccion){
+        if( jugadorAccion.puntosVida > 40){
+            jugadorAccion.puntosVida -=40;
+            this.estado(jugadorAccion);
+        }else{
+            jugadorAccion.puntosVida = 0;
+            console.error(jugadorAccion.nombre + " Murio!!");
+        }
+    }
+
+    this.estado = function (jugadorCurar){
+        console.info(this);
+        console.info(jugadorCurar);
+    }
+};
+
+/*La definicion de las LET = new son variables objeto de tipo jugador, las inicializa con sus valores definidos y la impresiones son por console.log, console.info, console.error*/
+
+let jugadorUno = new Jugador("Juan");
+let jugadorDos = new Jugador("Carlos");
+
+console.log(jugadorUno);
+console.log(jugadorDos);
+
+/*para utilizar los metodos .curar o .atacar debes poner por console por ejemplo: jugadorUno.curar(jugadorDos) o jugadorUno.atacar(jugadorDos)*/
